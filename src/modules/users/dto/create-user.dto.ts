5acac5291474a8b3d04b4006a14d0175bc8bd39a
Erick Saravia <erick@norttech.com>
2021-10-09 16:36:36 -0600

    feat(validationrequestpipe): added global pipe to validate all request api
    Return more specific messages when data provided is bad

--- src/modules/users/dto/create-user.dto.ts
+++ src/modules/users/dto/create-user.dto.ts
@@ -1,1 +1,48 @@
-export class CreateUserDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEnum, IsString } from 'class-validator';
+import { UserTypes } from '../entities/user.entity';
+
+export class CreateUserDto {
+  @ApiProperty({
+    name: 'type',
+    enum: UserTypes,
+  })
+  @IsEnum(UserTypes)
+  type: UserTypes;
+
+  @ApiProperty({
+    name: 'name',
+  })
+  @IsString()
+  name: string;
+
+  @IsString()
+  @ApiProperty({
+    name: 'lastName',
+  })
+  lastName: string;
+
+  @IsString()
+  @ApiProperty({
+    name: 'username',
+  })
+  username: string;
+
+  @IsString()
+  @ApiProperty({
+    name: 'dui',
+  })
+  dui: string;
+
+  @IsString()
+  @ApiProperty({
+    name: 'birthDate',
+  })
+  birthDate: string;
+
+  @IsString()
+  @ApiProperty({
+    name: 'password',
+  })
+  password: string;
+}
--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,1 +1,16 @@
-export class User {}
+export enum UserTypes {
+  Admin = `ADMIN`,
+  Customer = `CUSTOMER`,
+  Employee = `EMPLOYEE`,
+}
+
+export class User {
+  id: string;
+  dui: string;
+  birthDate: string;
+  name: string;
+  type: UserTypes;
+  lastName: string;
+  username: string;
+  createdAt: string;
+}
