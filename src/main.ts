d05a47b3b564230b3b9f3b63efc1e6601900a41f
Erick Saravia <erick@norttech.com>
2021-10-09 11:32:19 -0600

    feat(initial): creation Project
    Added development tools: dotenv, commitizen, husky\

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const port = process.env.PORT;
+  const app = await NestFactory.create(AppModule);
+  await app.listen(port, () => {
+    console.log(`Server Running on port: ${port}`);
+  });
+}
+bootstrap();
