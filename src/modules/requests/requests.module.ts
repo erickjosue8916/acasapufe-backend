9715702b03facd328baba4e46b9f8a73e2a41928
Erick Saravia <erick@norttech.com>
2021-10-12 19:26:22 -0600

    fix(requetsmodule): resolving dependencies with usersService

--- src/modules/requests/requests.module.ts
+++ src/modules/requests/requests.module.ts
@@ -4,10 +4,12 @@ import { RequestsController } from './requests.controller';
 import { MainDbService } from 'src/common/main-db/main-db.service';
 import { CustomersModule } from '../customers/customers.module';
 import { CustomersService } from '../customers/customers.service';
+import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/users.service';
 
 @Module({
-  imports: [CustomersModule],
+  imports: [CustomersModule, UsersModule],
   controllers: [RequestsController],
-  providers: [RequestsService, MainDbService, CustomersService],
+  providers: [RequestsService, MainDbService, CustomersService, UsersService],
 })
 export class RequestsModule {}
