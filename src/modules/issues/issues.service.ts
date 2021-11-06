1baf64763b80f49d221e768acdf6832e68340577
Erick Saravia <erick@norttech.com>
2021-11-05 20:11:17 -0600

    feat(issues): added customer data for every issue

--- src/modules/issues/issues.service.ts
+++ src/modules/issues/issues.service.ts
@@ -8,10 +8,12 @@ import { ISSUE_STATUS } from './entities/issue.entity';
 @Injectable()
 export class IssuesService {
   private collectionName: string;
+  private collectionCustomers: string;
   private collectionRef: FirebaseFirestore.CollectionReference<FirebaseFirestore.DocumentData>;
 
   constructor(private readonly dbService: MainDbService) {
     this.collectionName = dbService.collections.issues;
+    this.collectionCustomers = dbService.collections.customers;
     this.collectionRef = dbService.getCollection(this.collectionName);
   }
 
@@ -35,8 +37,32 @@ export class IssuesService {
 
   async findAll() {
     const result = await this.collectionRef.get();
+
     const items = await this.dbService.parseFirestoreItemsResponse(result);
-    return items;
+    const customerIds = items.reduce((customerIds, current) => {
+      const currentId = current.customerId;
+      if (!customerIds.includes(currentId)) customerIds.push(currentId);
+      return customerIds;
+    }, []);
+
+    const customers = [];
+    for await (const id of customerIds) {
+      const customerRequest = await this.dbService
+        .getCollection(this.collectionCustomers)
+        .doc(id)
+        .get();
+      const data = this.dbService.getDataFromDocument(customerRequest);
+      customers.push(data);
+    }
+
+    const response = items.map((item) => {
+      const customer = customers.filter(
+        (customer) => customer.id === item.customerId,
+      );
+      item.customer = customer;
+      return item;
+    });
+    return response;
   }
 
   async findOne(id: string) {
